b5dbbae295cb89c1571b1229afd6508e9452326f
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-04-02 00:08:50 +0530

    Solved problem

---
+++ src/main/java/org/mclods/orac/CountInfinity.java
@@ -0,0 +1,19 @@
+package org.mclods.orac;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://orac.amt.edu.au/problem/324/
+public class CountInfinity {
+    public static void sol() throws IOException {
+        int n;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=n; ++i) {
+            System.out.println(i);
+        }
+    }
+}
