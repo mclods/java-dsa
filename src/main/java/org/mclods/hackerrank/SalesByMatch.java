15fd81ab82b4dad6650732337fd6cc075d276918
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-04-07 21:07:48 +0530

    Solved problem

--- src/main/java/org/mclods/hackerrank/SalesByMatch.java
+++ src/main/java/org/mclods/hackerrank/SalesByMatch.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+// https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
 public class SalesByMatch {
     public static void sol() throws IOException {
         int n;
