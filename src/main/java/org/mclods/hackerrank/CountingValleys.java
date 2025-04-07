68ef76fc95630e68b174292f9d9aceacca2f5f87
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-04-07 21:26:20 +0530

    Solved problem

---
+++ src/main/java/org/mclods/hackerrank/CountingValleys.java
@@ -0,0 +1,34 @@
+package org.mclods.hackerrank;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// https://www.hackerrank.com/challenges/counting-valleys/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+public class CountingValleys {
+    public static void sol() throws IOException {
+        int n;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        String[] paths = br.readLine().split("");
+
+        int altLevel = 0, valleys = 0;
+
+        for(String i : paths) {
+            int currentLevel = altLevel;
+
+            if(i.equals("U")) {
+                altLevel++;
+            } else {
+                altLevel--;
+            }
+
+            if(altLevel < 0 && currentLevel == 0) {
+                valleys++;
+            }
+        }
+
+        System.out.println(valleys);
+    }
+}
