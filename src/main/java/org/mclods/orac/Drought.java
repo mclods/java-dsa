27ea9efe1dc62a78e2476affd3b253b2a421c0da
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-04-07 20:16:59 +0530

    Solved problem

---
+++ src/main/java/org/mclods/orac/Drought.java
@@ -0,0 +1,32 @@
+package org.mclods.orac;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+// https://orac.amt.edu.au/problem/340/
+public class Drought {
+    public static void sol() throws IOException {
+        int n, c, totalRainfall = 0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        c = Integer.parseInt(input[1]);
+
+        ArrayList<Integer> rainFallLevel = new ArrayList<>();
+        for(int i=0; i<n; ++i) {
+            rainFallLevel.add(Integer.parseInt(br.readLine()));
+        }
+
+        for(int i=0; i<n; ++i) {
+            totalRainfall += rainFallLevel.get(i);
+
+            if(totalRainfall >= c) {
+                System.out.println(i + 1);
+                break;
+            }
+        }
+    }
+}
