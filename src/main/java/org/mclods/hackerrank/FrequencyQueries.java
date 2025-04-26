3caa661746e6d054b2fd48f809ebf91286d2c834
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-04-27 00:23:15 +0530

    Solved Problem

--- src/main/java/org/mclods/hackerrank/FrequencyQueries.java
+++ src/main/java/org/mclods/hackerrank/FrequencyQueries.java
@@ -4,13 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 // https://www.hackerrank.com/challenges/frequency-queries/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
 public class FrequencyQueries {
-    private static void addToMap(Map<Integer, Integer> map, Integer key) {
+    private static void addCountToMap(Map<Integer, Integer> map, Integer key) {
         if(map.containsKey(key)) {
             map.put(key, map.get(key) + 1);
         } else {
@@ -18,12 +16,28 @@ public class FrequencyQueries {
         }
     }
 
-    private static void removeFromMap(Map<Integer, Integer> map, Integer key) {
+    private static void removeCountFromMap(Map<Integer, Integer> map, Integer key) {
         if(map.containsKey(key)) {
             int value = map.get(key);
             if(value > 0) {
                 map.put(key, value - 1);
             }
+        }
+    }
+
+    private static void updateFrequencyMap(HashMap<Integer, HashSet<Integer>> map,
+                                           Integer entry, Integer prevFrequency, Integer newFrequency) {
+        HashSet<Integer> frequencySet;
+        if(map.containsKey(newFrequency)) {
+            frequencySet = map.get(newFrequency);
+        } else {
+            frequencySet = new HashSet<>();
+        }
+        frequencySet.add(entry);
+        map.put(newFrequency, frequencySet);
+
+        if(prevFrequency != null && map.containsKey(prevFrequency)) {
+            map.get(prevFrequency).remove(entry);
         }
     }
 
@@ -33,22 +47,30 @@ public class FrequencyQueries {
 
         int q = Integer.parseInt(br.readLine());
         HashMap<Integer, Integer> dataMap = new HashMap<>();
+        HashMap<Integer, HashSet<Integer>> frequencyMap = new HashMap<>();
         ArrayList<Integer> outputs = new ArrayList<>();
 
         for(int i=0; i<q; ++i) {
             String[] inputs = br.readLine().split(" ");
             int operation = Integer.parseInt(inputs[0]);
             int data = Integer.parseInt(inputs[1]);
-
             switch (operation) {
-                case 1:
-                    addToMap(dataMap, data);
+                case 1: {
+                    Integer prevFrequency = dataMap.get(data);
+                    addCountToMap(dataMap, data);
+                    Integer newFrequency = dataMap.get(data);
+                    updateFrequencyMap(frequencyMap, data, prevFrequency, newFrequency);
                     break;
-                case 2:
-                    removeFromMap(dataMap, data);
+                }
+                case 2: {
+                    Integer prevFrequency = dataMap.get(data);
+                    removeCountFromMap(dataMap, data);
+                    Integer newFrequency = dataMap.get(data);
+                    updateFrequencyMap(frequencyMap, data, prevFrequency, newFrequency);
                     break;
+                }
                 case 3:
-                    if(dataMap.containsValue(data)) {
+                    if(frequencyMap.containsKey(data) && !frequencyMap.get(data).isEmpty()) {
                         outputs.add(1);
                     } else {
                         outputs.add(0);
