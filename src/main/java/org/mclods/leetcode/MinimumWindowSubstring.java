03d848c1063201e782a2a4ce910be1ea169e08cb
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-11-02 21:50:38 +0530

    Solved Problem

--- src/main/java/org/mclods/leetcode/MinimumWindowSubstring.java
+++ src/main/java/org/mclods/leetcode/MinimumWindowSubstring.java
@@ -3,14 +3,13 @@ package org.mclods.leetcode;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 // https://leetcode.com/problems/minimum-window-substring/
 public class MinimumWindowSubstring {
     private int getCharIndex(char c) {
-        if(c >= 65 && c <= 90) {
+        if (c >= 65 && c <= 90) {
             return c - 'A';
-        } else if(c >= 97 && c <= 122) {
+        } else if (c >= 97 && c <= 122) {
             return c - 'a' + 26;
         }
 
@@ -20,46 +19,47 @@ public class MinimumWindowSubstring {
     public String minWindow(String s1, String s2) {
         int n1 = s1.length(), n2 = s2.length();
 
-        if(n2 > n1) {
+        if (n2 > n1) {
             return "";
         }
 
+        // s1, s2 includes uppercase and lowercase letters
         final int FREQ_ARR_LEN = 52;
 
         int[] s2CharFreq = new int[FREQ_ARR_LEN];
-        for(int i=0; i<n2; ++i) {
+        for (int i = 0; i < n2; ++i) {
             char current = s2.charAt(i);
             s2CharFreq[getCharIndex(current)]++;
         }
 
         // neededChars = Number of different chars in s2
         int neededChars = 0;
-        for(int i : s2CharFreq) {
-            if(i > 0) {
+        for (int i : s2CharFreq) {
+            if (i > 0) {
                 neededChars++;
             }
         }
 
         int[] window = new int[FREQ_ARR_LEN];
-        int start = 0, end = 0, parsedChars = 0, minWindowLen = n1 + 1;
+        int start = 0, end = -1, parsedChars = 0, minWindowLen = n1 + 1;
         boolean startIsValid = false;
         String minWindowStr = "";
 
-        while(start < n1 && end < n1) {
-            if(startIsValid) {
+        while (start < n1 && end < n1) {
+            if (startIsValid) {
                 char endingChar = s1.charAt(end);
                 int endingCharIndex = getCharIndex(endingChar);
 
                 // s2 contains the ending character
-                if(s2CharFreq[endingCharIndex] > 0) {
+                if (s2CharFreq[endingCharIndex] > 0) {
                     window[endingCharIndex]++;
 
-                    if(s2CharFreq[endingCharIndex] == window[endingCharIndex]) {
+                    if (s2CharFreq[endingCharIndex] == window[endingCharIndex]) {
                         parsedChars++;
                     }
 
                     // Entire string s2 has been parsed
-                    if(parsedChars == neededChars) {
+                    if (parsedChars == neededChars) {
                         int windowLen = end - start + 1;
 
                         if (windowLen < minWindowLen) {
@@ -71,6 +71,12 @@ public class MinimumWindowSubstring {
                         int startingCharIndex = getCharIndex(startingChar);
 
                         window[startingCharIndex]--;
+
+                        // Check and adjust parsed chars
+                        if (window[startingCharIndex] < s2CharFreq[startingCharIndex]) {
+                            parsedChars--;
+                        }
+
                         start++;
                         startIsValid = false;
                     } else {
@@ -83,13 +89,34 @@ public class MinimumWindowSubstring {
                 char startingChar = s1.charAt(start);
                 int startingCharIndex = getCharIndex(startingChar);
 
-                // s2 contains the starting character, reset the window
-                if(s2CharFreq[startingCharIndex] > 0) {
-                    parsedChars = 0;
-                    end = start;
-                    Arrays.fill(window, 0);
+                // s2 contains the starting character
+                if (s2CharFreq[startingCharIndex] > 0) {
+                    if(start <= end) {
+                        // Check if new window already contains all parsedChars or not
+                        if (parsedChars == neededChars) {
+                            int windowLen = end - start + 1;
 
-                    startIsValid = true;
+                            if (windowLen < minWindowLen) {
+                                minWindowLen = windowLen;
+                                minWindowStr = s1.substring(start, end + 1);
+                            }
+
+                            window[startingCharIndex]--;
+
+                            if (window[startingCharIndex] < s2CharFreq[startingCharIndex]) {
+                                parsedChars--;
+                            }
+
+                            start++;
+                        } else {
+                            end++;
+                            startIsValid = true;
+                        }
+                    } else {
+                        end = start;
+                        parsedChars = 0;
+                        startIsValid = true;
+                    }
                 } else {
                     start++;
                 }
