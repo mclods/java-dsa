b7672111a2f0fd14e69527d1e038484093e4a605
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2025-12-30 00:26:09 +0530

    Solved Problem

---
+++ src/main/java/org/mclods/leetcode/RemovingStarsFromAString.java
@@ -0,0 +1,39 @@
+package org.mclods.leetcode;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+// https://leetcode.com/problems/removing-stars-from-a-string
+public class RemovingStarsFromAString {
+    public String removeStars(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(int i=0; i<s.length(); ++i) {
+            char current = s.charAt(i);
+
+            if(current == '*') {
+                stack.poll();
+            } else {
+                stack.push(current);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.removeLast());
+        }
+
+        return sb.toString();
+    }
+
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        String output = new RemovingStarsFromAString().removeStars(input);
+        System.out.println(output);
+    }
+}
